721b1bde3ba08289cea7745ee447ba4bc99cc757
igorpadilha980 <igor.padilha2004@gmail.com>
2023-01-21 19:09:31 -0300

    update: export enviroment variables

--- src/System.h
+++ src/System.h
@@ -2,4 +2,18 @@
 
 #include "Logger.h"
 
-#define API_URL "http://e-labkey.000webhostapp.com/api"
+#ifndef LAB_NUMBER
+    #define LAB_NUMBER 418
+#endif
+
+#ifndef AUTH_API_URL
+    #define AUTH_API_URL "http://e-labkey.000webhostapp.com/api"
+#endif
+
+#ifndef WIFI_SSID
+    #define WIFI_SSID "devwifi"
+#endif
+
+#ifndef WIFI_PASSWORD
+    #define WIFI_PASSWORD "openthedoor78"
+#endif
