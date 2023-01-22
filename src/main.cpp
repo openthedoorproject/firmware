268c2c9ec19381d8b182b6a6df1f0993fd1f01ee
igorpadilha980 <igor.padilha2004@gmail.com>
2023-01-22 18:03:04 -0300

    fix: pinout conflict

--- src/main.cpp
+++ src/main.cpp
@@ -97,7 +97,7 @@ void setup()
 
     Display::Init(0x27, 2, 16);
 
-    CardReader::Init(16, 14);
+    CardReader::Init(16, 13);
 
     Auth::Init(AUTH_API_URL, LAB_NUMBER, WIFI_SSID, WIFI_PASSWORD);
 
